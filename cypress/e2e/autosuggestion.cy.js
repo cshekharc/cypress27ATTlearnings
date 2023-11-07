10b63457c7073faedbfa87c77708d7082d69a8a6
cshekharc <cshekhars.choudhari@gmail.com>
2023-11-07 22:02:54 +0530

    changes in autosuggetiosn and added iframe spec

--- cypress/e2e/autosuggestion.cy.js
+++ cypress/e2e/autosuggestion.cy.js
@@ -4,13 +4,13 @@ describe("auto suggetions for food items",()=>{
     it("validate the food item selected",()=>{
         cy.visit("https://www.webdriveruniversity.com/")
         cy.get("#autocomplete-textfield").invoke("removeAttr","target").click()
-        cy.get("[placeholder='Food Item']").type("c")
+        cy.get("[placeholder='Food Item']").type("p")
         cy.get(".autocomplete-items >div").each((foodItem)=>{
-            if(foodItem.text()==="Chicken"){
+            if(foodItem.text()==="Pancakes"){
                 cy.wrap(foodItem).click()
                 //foodItem.click()
                 cy.get("#submit-button").click()
-                cy.url().should("include", "Chicken")
+                cy.url().should("include", "Pancakes")
             }
 
         })
---
+++ cypress/e2e/iframes.cy.js
@@ -0,0 +1,15 @@
+///<reference types="cypress"/>
+
+describe("validating iframe modal",()=>{
+    it("Verify the message on modal",()=>{
+        cy.visit("https://www.webdriveruniversity.com/index.html")
+        cy.get("#iframe").invoke("removeAttr", "target").click()
+        cy.get("#frame").then((newFrame)=>{
+            const frame = newFrame.contents().find("body")
+            cy.wrap(frame).as("thiswe")
+            cy.get("@thiswe").find('[data-target="#myModal"]').click()
+            cy.get("@thiswe").find("div.modal-body > p")
+            .should("include.text", "laptops, game consoles, cameras...")
+        })
+    })
+})
